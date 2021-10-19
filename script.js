96e9a444d5a6e948bce7f4340aba85e05abffc9c
AKACIAflower <lgs2722@gmail.com>
2021-10-19 13:50:21 +0900

    getMealsBySearch and mealInfoPopup
    getMealsBySearch and mealInfoPopup

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const meals = document.getElementById("meals");
+const mealsEl = document.getElementById("meals");
 const favoriteContainer = document.getElementById("fav-meals");
+
+const mealPopup = document.getElementById("meal-popup");
+const mealInfoEl = document.getElementById("meal-information");
+const popupCloseBtn = document.getElementById("close-popup");
+
+const searchTerm = document.getElementById("search-term");
+const searchBtn = document.getElementById("search");
 
 getRandomMeal();
 fetchFavMeals();
@@ -11,6 +18,8 @@ async function getRandomMeal() {
 
   const respData = await resp.json();
   const randomMeal = respData.meals[0];
+
+  console.log(randomMeal);
 
   addMeal(randomMeal, true);
 }
@@ -27,9 +36,14 @@ async function getMealById(id) {
 }
 
 async function getMealsBySearch(term) {
-  const meals = await fetch(
+  const resp = await fetch(
     "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
   );
+
+  const respData = await resp.json();
+  const meals = respData.meals;
+
+  return meals;
 }
 
 function addMeal(mealData, random = false) {
@@ -64,7 +78,11 @@ function addMeal(mealData, random = false) {
     fetchFavMeals();
   });
 
-  meals.appendChild(meal);
+  meal.addEventListener("click", () => {
+    showMealInfo(mealData);
+  });
+
+  mealsEl.appendChild(meal);
 }
 
 function addMealLS(mealId) {
@@ -104,6 +122,7 @@ async function fetchFavMeals() {
 
 function addMealToFav(mealData) {
   const favMeal = document.createElement("li");
+
   favMeal.classList.add("fav-meal");
   favMeal.innerHTML = `
       <img
@@ -121,5 +140,64 @@ function addMealToFav(mealData) {
     fetchFavMeals();
   });
 
+  favMeal.addEventListener("click", () => {
+    showMealInfo(mealData);
+  });
+
   favoriteContainer.appendChild(favMeal);
 }
+
+function showMealInfo(mealData) {
+  // clean it up
+  mealInfoEl.innerHTML = "";
+
+  // update the Meal info
+  const mealEl = document.createElement("div");
+
+  const ingredients = [];
+  // get ingredients and measures
+  for (let i = 1; i <= 20; i++) {
+    if (mealData["strIngredient" + i]) {
+      ingredients.push(
+        `${mealData["strIngredient" + i]} - ${mealData["strMeasure" + i]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  mealEl.innerHTML = `
+      <h1>${mealData.strMeal}</h1>
+      <img
+        src="${mealData.strMealThumb}"
+        alt="${mealData.strMeal}"
+      />
+      <p>
+      ${mealData.strInstructions}
+      </p>
+      <h3>Ingredients:</h3>
+      <ul>
+        ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+      </ul>
+  `;
+
+  mealInfoEl.appendChild(mealEl);
+
+  // show the popup
+  mealPopup.classList.remove("hidden");
+}
+
+searchBtn.addEventListener("click", async () => {
+  // Clean container
+  mealsEl.innerHTML = "";
+
+  const search = searchTerm.value;
+
+  const meals = await getMealsBySearch(search);
+
+  if (meals) meals.forEach((meal) => addMeal(meal));
+});
+
+popupCloseBtn.addEventListener("click", () => {
+  mealPopup.classList.add("hidden");
+});
